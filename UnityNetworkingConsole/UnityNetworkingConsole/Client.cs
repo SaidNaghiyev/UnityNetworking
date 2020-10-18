5794781a3ad632f5c4d1173e4eb3947d0c849a33
SaidNaghiyev <SaidNaghiyev@github.com>
2020-10-18 12:03:59 +0400

    fgdgf

--- UnityNetworkingConsole/UnityNetworkingConsole/Client.cs
+++ UnityNetworkingConsole/UnityNetworkingConsole/Client.cs
@@ -1,10 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Net;
+using System.Net.Sockets;
 
 namespace UnityNetworkingConsole
 {
     class Client
     {
+        public static int dataBufferSize = 
+        public int id;
+        public TCP tcp;
+
+        public class TCP
+        {
+            public TcpClient socket;
+
+            private readonly int id;
+
+            public TCP(int _id)
+            {
+                id = _id;
+            }
+
+            public void Connect(TcpClient _socket)
+            {
+                socket = _socket;
+                socket.ReceiveBufferSize = databuf
+            }
+        }
     }
 }
