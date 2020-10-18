8e7933322232972114ed4c706d7ff7806152c39a
SaidNaghiyev <SaidNaghiyev@github.com>
2020-10-18 11:43:44 +0400

    Console

---
+++ UnityNetworkingConsoleApp/UnityNetworkingConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnityNetworkingConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
