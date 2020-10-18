fd8fd97b1337cbdfab258d73b3ded15474ccc958
SaidNaghiyev <SaidNaghiyev@github.com>
2020-10-18 11:52:17 +0400

    Create Server.cs

---
+++ UnityNetworkingConsole/UnityNetworkingConsole/Server.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UnityNetworkingConsole
+{
+    class Server
+    {
+        public static int MaxPlayer { get; private set; }
+        public static int Port { get; private set; }
+
+        private static TcpListener tcpListener;
+
+        public static void Start(int _maxPlayer, int _port)
+        {
+            MaxPlayer = _maxPlayer;
+            Port = _port;
+
+            tcpListener = new TcpListener(IPAddress.Any, Port);
+        }
+
+    }
+}
