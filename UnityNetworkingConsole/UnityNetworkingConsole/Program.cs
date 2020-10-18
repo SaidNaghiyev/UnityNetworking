4220e25ebbe19686c7f48479e425ca50ffa42566
SaidNaghiyev <SaidNaghiyev@github.com>
2020-10-18 11:47:58 +0400

    Code

--- UnityNetworkingConsole/UnityNetworkingConsole/Program.cs
+++ UnityNetworkingConsole/UnityNetworkingConsole/Program.cs
@@ -6,7 +6,8 @@ namespace UnityNetworkingConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Title = "Game Server";
+            Console.ReadKey();
         }
     }
 }
